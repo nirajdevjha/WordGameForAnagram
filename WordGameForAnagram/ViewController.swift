f65f903bc654ea88d8410443f73f1f43387640e3
nirajcodingmart <niraj@codingmart.com>
2019-03-28 17:55:16 +0530

    Initial Commit

---
+++ WordGameForAnagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordGameForAnagram
+//
+//  Created by Niraj Jha on 28/03/19.
+//  Copyright © 2019 Niraj Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
